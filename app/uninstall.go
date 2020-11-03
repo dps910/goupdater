7bfa9cede449f8b0ee6518a46d80c53d73a34908
david <42849369+dps910@users.noreply.github.com>
2020-11-03 16:27:14 +0000

    Add flag and dir

--- app/uninstall.go
+++ app/uninstall.go
@@ -3,11 +3,18 @@ package app
 import (
   "fmt"
   "os"
+  "flag"
+)
+
+var (
+  c = flag.Int("-c", 0755, "chmod number")
+  // just assuming the dir for now
+  DIR = "/usr/local/go"
 )
 
 func Uninstall() error {
   // check directory exists
-  dir, err := ("/usr/local/go"); os.isNotExist(err) {
+  dir, err := (DIR; os.isNotExist(err) {
     return errors.New("go directory does not exist")
   }
 
@@ -16,7 +23,7 @@ func Uninstall() error {
   base8perm := os.Sprintf("%o\n", perm)
 
   if base8perm < string(433) {
-    
+    Chmod(DIR, os.FileMode(c))
   }
 
   // If go directory does not exist, install golang to that directory
