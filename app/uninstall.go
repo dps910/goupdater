efa9c173a3620ac2cbbea699f98d281b1cedf769
david <42849369+dps910@users.noreply.github.com>
2020-11-03 22:26:05 +0000

    Uninstall golang

--- app/uninstall.go
+++ app/uninstall.go
@@ -1,34 +1,63 @@
 package app
 
 import (
-  "fmt"
-  "os"
-  "flag"
+	"bufio"
+	"errors"
+	"flag"
+	"fmt"
+	"os"
 )
 
 var (
-  c = flag.Int("-c", 0755, "chmod number")
-  // just assuming the dir for now
-  DIR = "/usr/local/go"
+	c   = flag.Int("-c", 0755, "chmod number")
+	dir = flag.String("-dir", "", "Use this flag if the golang directories are somewhere else.")
+	// DIR = just assuming the dir for now
+	DIR = "/usr/local/go"
+	// GOPATH specifies the GOPATH in "go env"
+	GOPATH = "$HOME/go"
 )
 
+// RemoveDir removes directories
+func RemoveDir(dir string) error {
+	err := os.RemoveAll(dir)
+	if err != nil {
+		// If there is an error here, specify the directories instead
+		str := bufio.NewReader(os.Stdin)
+
+		fmt.Println("Couldn't find directories. Specify directories to delete.")
+
+		txt, _ := str.ReadString('\n')
+
+		// Remove directory specified
+		os.RemoveAll(txt)
+	}
+	fmt.Printf("Deleted Go directories %s and %s", DIR, GOPATH)
+	return nil
+}
+
+// Uninstall golang
 func Uninstall() error {
-  // check directory exists
-  dir, err := (DIR; os.isNotExist(err) {
-    return errors.New("go directory does not exist")
-  }
+	// check directory exists
+	dir, err := os.Stat(DIR)
+	if os.IsNotExist(err) {
+		return errors.New("Directory doesn't exist")
+	}
 
-  perm := dir.Mode().Perm
+	perm := dir.Mode().Perm()
 
-  base8perm := os.Sprintf("%o\n", perm)
+	base8perm := fmt.Sprintf("%o\n", perm)
 
-  if base8perm < string(433) {
-    Chmod(DIR, os.FileMode(c))
-  }
+	if base8perm < string(433) {
+		Chmod(DIR, os.FileMode(*c))
+	}
 
-  // If go directory does not exist, install golang to that directory
-  Install()
+	// Check if is dir and not a file, should return true
+	if dir.IsDir() {
+		// Remove directory
+		RemoveDir(DIR)
+		RemoveDir(GOPATH)
+		fmt.Println("Deleted golang directory :)")
+	}
 
-  
-  return nil
+	return nil
 }
