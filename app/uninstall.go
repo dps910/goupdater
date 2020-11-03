f7ebcd1d9210a162e733ad33e7037a2fd66b3bdf
david <42849369+dps910@users.noreply.github.com>
2020-11-03 14:22:07 +0000

    check perms

--- app/uninstall.go
+++ app/uninstall.go
@@ -7,8 +7,16 @@ import (
 
 func Uninstall() error {
   // check directory exists
-  _, err := ("/usr/local/go"); os.isNotExist(err) {
+  dir, err := ("/usr/local/go"); os.isNotExist(err) {
     return errors.New("go directory does not exist")
+  }
+
+  perm := dir.Mode().Perm
+
+  base8perm := os.Sprintf("%o\n", perm)
+
+  if base8perm < string(433) {
+    
   }
 
   // If go directory does not exist, install golang to that directory
