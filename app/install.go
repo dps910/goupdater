755601cfc0a5676c4bbe006b9a7307d3bea4cc26
david <42849369+dps910@users.noreply.github.com>
2020-11-05 22:34:47 +0000

    install

--- app/install.go
+++ app/install.go
@@ -1,39 +1,51 @@
 package app
 
 import (
+	"archive/tar"
 	"bufio"
+	"compress/gzip"
 	"errors"
+	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"log"
 	"os"
 )
 
 var (
-	dir = "$HOME/test"
+	extract = flag.String("-tarfile", "", "Extract given tar file to directory specified")
 )
 
-func Install() error {
-	info, err := os.Stat("dir")
+func init() {
+	flag.Parse()
+}
+
+// Install golang to directory
+func Install(dir string, r io.Reader) error {
+	// Check that tar file exists
+
+	info, err := os.Stat(dir)
 	if err != nil {
 		return errors.New("Couldn't get info of folder")
 	}
 
 	// Check if dir, if not return error
 	if !info.IsDir() {
-		return errors.New("This is not a directory.")
+		return errors.New("This is not a directory")
 	}
 
 	// Check permissions, make sure that we have permission to read dir
 	perm := info.Mode().Perm()
 
 	// Make sure to print in octal representation (base 8) instead of base 10 https://stackoverflow.com/a/41259532
-	base8perm := fmt.Sprintf("%o\n", perm)
+	base8perm := fmt.Sprintf("%o", perm)
 
 	// 433 is r---wx-wx which I think would be the lowest chmod for reading.
-	if base8perm < string(433) {
+	if base8perm != "755" {
 		// Get user input from stdin
 		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Do you want to change permissions to 755? [y/n]")
+		fmt.Printf("Do you want to change permissions to 755? [y/n] Current permissions: %s", perm)
 
 		// Read string
 		text, _ := reader.ReadString('\n')
@@ -50,20 +62,58 @@ func Install() error {
 			os.Exit(0)
 		}
 
-		// Chcek if /usr/local/go exists, if it does then delete it (not going to yet, but when this is fully working)
-		godir := "~/test"
-		_, err := ioutil.ReadDir(godir)
+		// Check if go directory exists, if it does then delete it (not going to yet, but when this is fully working)
+		_, err := ioutil.ReadDir(dir)
 		if err != nil {
 			return errors.New("Directory doesn't exist")
 		}
+		fmt.Println("Directory does exist")
 
-		// If directory does exist, delete directory
-		_ = os.Remove(godir)
+		// If go directory does exist, then we extract tar.gz file to /tmp temporarily and then move it
+		readGzip, err := gzip.NewReader(r)
+		if err != nil {
+			return errors.New("Couldn't read file")
+		}
+		defer readGzip.Close()
+
+		// Reader loops over tar file and creates the file structure in dir
+		// Read files in archive
+		tarfile := tar.NewReader(readGzip)
+		for {
+			header, err := tarfile.Next()
+
+			switch {
+			// If entries in the tar file are finished, call EOF()
+			case err == io.EOF:
+				fmt.Printf("Reached end of archive, size %d", header.Size)
+				break
+			case err != nil:
+				return errors.New("tar.gz is nil")
+			}
+
+			switch header.Typeflag {
+			// Directory
+			case tar.TypeDir:
+				if err := os.Mkdir(dir, 0755); err != nil {
+					log.Fatalln(err)
+				}
+
+			// File
+			case tar.TypeReg:
+				// Open files in header
+				f, err := os.OpenFile(header.Name, os.O_CREATE|os.O_RDWR, 0755)
+				if err != nil {
+					fmt.Println("Couldn't create file")
+				}
+
+				c, err := io.Copy(f, tarfile)
+				if err != nil {
+					fmt.Println("Couldn't copy read file to f")
+				}
+				fmt.Println(c)
+			}
+		}
 	}
 
 	return nil
-}
-
-func main() {
-	Install()
 }
